4683c5789c4ae0e90734da070702786207a529be
Tomas Baca <tomas.baca@fel.cvut.cz>
2019-08-19 10:58:26 +0200

    added vertical threshold to bumper visualization

--- include/mrs_bumper/mrs_bumper_display.h
+++ include/mrs_bumper/mrs_bumper_display.h
@@ -1,3 +1,4 @@
+// clang: MatousFormat
 /*
  * Copyright (c) 2012, Willow Garage, Inc.
  * All rights reserved.
@@ -39,94 +40,95 @@
 
 namespace Ogre
 {
-class SceneNode;
+  class SceneNode;
 }
 
 namespace rviz
 {
-class ColorProperty;
-class EnumProperty;
-class FloatProperty;
-class IntProperty;
-class BoolProperty;
-}
+  class ColorProperty;
+  class EnumProperty;
+  class FloatProperty;
+  class IntProperty;
+  class BoolProperty;
+}  // namespace rviz
 
 // All the source in this plugin is in its own namespace.  This is not
 // required but is good practice.
 namespace mrs_rviz_plugins
 {
 
-class MRS_Bumper_Visual;
+  class MRS_Bumper_Visual;
 
-// BEGIN_TUTORIAL
-// Here we declare our new subclass of rviz::Display.  Every display
-// which can be listed in the "Displays" panel is a subclass of
-// rviz::Display.
-//
-// MRS_Bumper_Display will show a 3D arrow showing the direction and magnitude
-// of the IMU acceleration vector.  The base of the arrow will be at
-// the frame listed in the header of the MRS_Bumper_ message, and the
-// direction of the arrow will be relative to the orientation of that
-// frame.  It will also optionally show a history of recent
-// acceleration vectors, which will be stored in a circular buffer.
-//
-// The MRS_Bumper_Display class itself just implements the circular buffer,
-// editable parameters, and Display subclass machinery.  The visuals
-// themselves are represented by a separate class, MRS_Bumper_Visual.  The
-// idiom for the visuals is that when the objects exist, they appear
-// in the scene, and when they are deleted, they disappear.
-class MRS_Bumper_Display: public rviz::MessageFilterDisplay<mrs_msgs::ObstacleSectors>
-{
-Q_OBJECT
-public:
-  // Constructor.  pluginlib::ClassLoader creates instances by calling
-  // the default constructor, so make sure you have one.
-  MRS_Bumper_Display();
-  virtual ~MRS_Bumper_Display();
+  // BEGIN_TUTORIAL
+  // Here we declare our new subclass of rviz::Display.  Every display
+  // which can be listed in the "Displays" panel is a subclass of
+  // rviz::Display.
+  //
+  // MRS_Bumper_Display will show a 3D arrow showing the direction and magnitude
+  // of the IMU acceleration vector.  The base of the arrow will be at
+  // the frame listed in the header of the MRS_Bumper_ message, and the
+  // direction of the arrow will be relative to the orientation of that
+  // frame.  It will also optionally show a history of recent
+  // acceleration vectors, which will be stored in a circular buffer.
+  //
+  // The MRS_Bumper_Display class itself just implements the circular buffer,
+  // editable parameters, and Display subclass machinery.  The visuals
+  // themselves are represented by a separate class, MRS_Bumper_Visual.  The
+  // idiom for the visuals is that when the objects exist, they appear
+  // in the scene, and when they are deleted, they disappear.
+  class MRS_Bumper_Display : public rviz::MessageFilterDisplay<mrs_msgs::ObstacleSectors>
+  {
+    Q_OBJECT
+  public:
+    // Constructor.  pluginlib::ClassLoader creates instances by calling
+    // the default constructor, so make sure you have one.
+    MRS_Bumper_Display();
+    virtual ~MRS_Bumper_Display();
 
-  // Overrides of protected virtual functions from Display.  As much
-  // as possible, when Displays are not enabled, they should not be
-  // subscribed to incoming data and should not show anything in the
-  // 3D view.  These functions are where these connections are made
-  // and broken.
-protected:
-  virtual void onInitialize();
+    // Overrides of protected virtual functions from Display.  As much
+    // as possible, when Displays are not enabled, they should not be
+    // subscribed to incoming data and should not show anything in the
+    // 3D view.  These functions are where these connections are made
+    // and broken.
+  protected:
+    virtual void onInitialize();
 
-  // A helper to clear this display back to the initial state.
-  virtual void reset();
+    // A helper to clear this display back to the initial state.
+    virtual void reset();
 
-  // These Qt slots get connected to signals indicating changes in the user-editable properties.
-private Q_SLOTS:
-  void updateColorAndAlpha();
-  void updateHistoryLength();
-  void updateDisplayMode();
-  void updateShowUndetected();
-  void updateShowNoData();
-  void updateCollisions();
+    // These Qt slots get connected to signals indicating changes in the user-editable properties.
+  private Q_SLOTS:
+    void updateColorAndAlpha();
+    void updateHistoryLength();
+    void updateDisplayMode();
+    void updateShowUndetected();
+    void updateShowNoData();
+    void updateCollisions();
 
-  // Function to handle an incoming ROS message.
-private:
-  void processMessage( const mrs_msgs::ObstacleSectors::ConstPtr& msg );
+    // Function to handle an incoming ROS message.
+  private:
+    void processMessage(const mrs_msgs::ObstacleSectors::ConstPtr& msg);
 
-  // Storage for the list of visuals.  It is a circular buffer where
-  // data gets popped from the front (oldest) and pushed to the back (newest)
-  boost::circular_buffer<boost::shared_ptr<MRS_Bumper_Visual> > visuals_;
+    // Storage for the list of visuals.  It is a circular buffer where
+    // data gets popped from the front (oldest) and pushed to the back (newest)
+    boost::circular_buffer<boost::shared_ptr<MRS_Bumper_Visual>> visuals_;
 
-  // User-editable property variables.
-  rviz::ColorProperty* color_property_;
-  rviz::FloatProperty* alpha_property_;
-  rviz::BoolProperty* collision_colorize_property_;
-  rviz::FloatProperty* collision_threshold_property_;
-  rviz::FloatProperty* collision_alpha_property_;
-  rviz::ColorProperty* collision_color_property_;
-  rviz::IntProperty* history_length_property_;
-  rviz::EnumProperty* display_mode_property_;
-  rviz::BoolProperty* show_undetected_property_;
-  rviz::BoolProperty* show_no_data_property_;
-};
-// END_TUTORIAL
+    // User-editable property variables.
+    rviz::ColorProperty* color_property_;
+    rviz::FloatProperty* alpha_property_;
+    rviz::BoolProperty* collision_colorize_property_;
+    rviz::FloatProperty* horizontal_collision_threshold_property_;
+    rviz::FloatProperty* vertical_collision_threshold_property_;
+    rviz::FloatProperty* collision_alpha_property_;
+    rviz::ColorProperty* collision_color_property_;
+    rviz::IntProperty* history_length_property_;
+    rviz::EnumProperty* display_mode_property_;
+    rviz::BoolProperty* show_undetected_property_;
+    rviz::BoolProperty* show_no_data_property_;
+  };
+  // END_TUTORIAL
 
-} // end namespace mrs_rviz_plugins
+}  // end namespace mrs_rviz_plugins
 
-#endif // IMU_DISPLAY_H
-// %EndTag(FULL_SOURCE)%
+#endif  // IMU_DISPLAY_H
+        // %EndTag(FULL_SOURCE)%
