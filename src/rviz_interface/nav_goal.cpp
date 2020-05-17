0bf353aaf6c50f3cdca5d8582fb8f4f9aee2566f
Tomas Baca <klaxalk@gmail.com>
2020-05-17 20:54:51 +0200

    added rviz interface

---
+++ src/rviz_interface/nav_goal.cpp
@@ -0,0 +1,266 @@
+/* includes //{ */
+
+#include <ros/ros.h>
+#include <nodelet/nodelet.h>
+
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2/LinearMath/Quaternion.h>
+
+#include <geometry_msgs/PoseStamped.h>
+
+#include <nav_msgs/Odometry.h>
+
+#include <mrs_msgs/MpcTrackerDiagnostics.h>
+#include <mrs_msgs/ReferenceStampedSrv.h>
+
+#include <mrs_lib/param_loader.h>
+#include <mrs_lib/profiler.h>
+#include <mrs_lib/transformer.h>
+#include <mrs_lib/mutex.h>
+
+//}
+
+namespace mrs_rviz_plugins
+{
+
+namespace rviz_interface
+{
+
+/* class NavGoal //{ */
+
+class NavGoal : public nodelet::Nodelet {
+
+public:
+  virtual void onInit();
+
+private:
+  ros::NodeHandle nh_;
+  bool            is_initialized_ = false;
+
+  // | ---------------------- msg callbacks --------------------- |
+
+private:
+  void                       callbackRvizNavGoal(const geometry_msgs::PoseStampedConstPtr& msg);
+  ros::Subscriber            sub_rviz_nav_goal_;
+  geometry_msgs::PoseStamped rviz_nav_goal_;
+  std::mutex                 mutex_rviz_nav_goal_;
+  int                        seq_ = 0;
+
+  void               callbackOdomUav(const nav_msgs::OdometryConstPtr& msg);
+  nav_msgs::Odometry odom_uav_;
+  bool               got_odom_uav_ = false;
+  std::mutex         mutex_odom_uav_;
+
+  void               callbackTrackerSetpoint(const nav_msgs::OdometryConstPtr& msg);
+  nav_msgs::Odometry tracker_setpoint_;
+  bool               got_tracker_setpoint_ = false;
+  std::mutex         mutex_tracker_setpoint_;
+
+private:
+  mrs_lib::Profiler* profiler_;
+  bool               _profiler_enabled_ = false;
+};
+
+//}
+
+/* onInit() //{ */
+
+void NavGoal::onInit() {
+
+  /* obtain node handle */
+  nh_ = nodelet::Nodelet::getMTPrivateNodeHandle();
+
+  /* waits for the ROS to publish clock */
+  ros::Time::waitForValid();
+
+  // | ------------------- load ros parameters ------------------ |
+  mrs_lib::ParamLoader param_loader(nh_, "NavGoal");
+
+  param_loader.loadParam("enable_profiler", _profiler_enabled_);
+
+  // | ------------------ initialize subscribers ----------------- |
+  sub_rviz_nav_goal_ = nh_.subscribe("rviz_nav_goal_in", 1, &NavGoal::callbackRvizNavGoal, this, ros::TransportHints().tcpNoDelay());
+
+  // | ----------------------- finish init ---------------------- |
+  is_initialized_ = true;
+
+  ROS_INFO_ONCE("[NavGoal]: initialized");
+
+  ROS_INFO("[RvizPoseEstimate]: Waiting for user input in rviz...");
+}
+//}
+
+// | ---------------------- msg callbacks --------------------- |
+
+/* callbackRvizNavGoal() //{ */
+
+void NavGoal::callbackRvizNavGoal(const geometry_msgs::PoseStampedConstPtr& msg) {
+
+  /* do not continue if the nodelet is not initialized */
+  if (!is_initialized_)
+    return;
+
+  got_odom_uav_ = false;
+
+  geometry_msgs::PoseStamped rviz_nav_goal_tmp = *msg;
+
+  ROS_INFO("[NavGoal]: Received rviz nav goal msg #%d", seq_ + 1);
+
+  // Get uav name of the pose estimate frame
+  size_t      idx_slash = msg->header.frame_id.find("/");
+  std::string uav_name  = msg->header.frame_id.substr(0, idx_slash);
+
+  ROS_INFO("[NavGoal]: Resolved UAV name to: %s", uav_name.c_str());
+
+  ros::Subscriber sub_odom_uav =
+      nh_.subscribe("/" + uav_name + "/control_manager/cmd_odom", 1, &NavGoal::callbackOdomUav, this, ros::TransportHints().tcpNoDelay());
+
+  // Wait 1s for UAV odometry
+  ros::Duration timeout(1.0);
+  ros::Duration total_sleep_time(0.0);
+  while (!got_odom_uav_ && total_sleep_time < timeout) {
+    ros::Duration sleep_time(0.01);
+    sleep_time.sleep();
+    total_sleep_time += sleep_time;
+  }
+
+  if (total_sleep_time >= timeout) {
+    ROS_INFO("[NavGoal]: UAV odometry %f s timeout.", total_sleep_time.toSec());
+    got_odom_uav_         = false;
+    got_tracker_setpoint_ = false;
+    return;
+  }
+  ROS_INFO("[NavGoal]: Waited %f s for UAV odometry.", total_sleep_time.toSec());
+
+  double setpoint_z;
+
+  // Set setpoint height to current height for global frames (ending with origin)
+  // Set setpoint height to 0 for body-fixed frames
+  std::size_t found = msg->header.frame_id.find("_origin");
+  if (found != std::string::npos) {
+    std::scoped_lock lock(mutex_odom_uav_);
+
+    setpoint_z = odom_uav_.pose.pose.position.z;
+  } else {
+    setpoint_z = 0.0;
+  }
+
+  // Create new waypoint msg
+  mrs_msgs::ReferenceStampedSrv new_waypoint;
+  new_waypoint.request.header.seq           = seq_;
+  new_waypoint.request.header.frame_id      = rviz_nav_goal_tmp.header.frame_id;
+  new_waypoint.request.header.stamp         = ros::Time::now();
+  new_waypoint.request.reference.position.x = rviz_nav_goal_tmp.pose.position.x;
+  new_waypoint.request.reference.position.y = rviz_nav_goal_tmp.pose.position.y;
+  new_waypoint.request.reference.position.z = setpoint_z;
+  new_waypoint.request.reference.heading    = mrs_lib::AttitudeConverter(rviz_nav_goal_tmp.pose.orientation).getYaw();
+
+  ros::ServiceClient srv_client_reference = nh_.serviceClient<mrs_msgs::ReferenceStampedSrv>("/" + uav_name + "/control_manager/reference");
+
+  ROS_INFO("[NavGoal]: Calling reference service: %s", ("/" + uav_name + "/control_manager/reference").c_str());
+  srv_client_reference.call(new_waypoint);
+  ROS_INFO("[NavGoal]: Reference service response: %s", new_waypoint.response.message.c_str());
+
+  if (!new_waypoint.response.success) {
+    ROS_ERROR("[NavGoal]: Could not set reference.");
+    got_odom_uav_         = false;
+    got_tracker_setpoint_ = false;
+    return;
+  }
+
+  ros::Subscriber sub_setpoint_odom = nh_.subscribe("/" + uav_name + "/control_manager/mpc_tracker/setpoint_odom", 1, &NavGoal::callbackTrackerSetpoint, this,
+                                                    ros::TransportHints().tcpNoDelay());
+
+  // Wait 1s for setpoint odometry
+  ros::Duration total_setpoint_sleep_time(0.0);
+  while (!got_tracker_setpoint_ && total_setpoint_sleep_time < ros::Duration(1.0)) {
+    ros::Duration sleep_time(0.01);
+    sleep_time.sleep();
+    total_setpoint_sleep_time += sleep_time;
+  }
+  ROS_INFO("[NavGoal]: Waited %f s for setpoint odometry.", total_setpoint_sleep_time.toSec());
+
+  // Wait 1s for tracking start
+  ros::Duration total_diag_sleep_time(0.0);
+  while (total_diag_sleep_time < ros::Duration(1.0)) {
+
+    geometry_msgs::Pose tracker_pose_tmp;
+    {
+      std::scoped_lock lock(mutex_tracker_setpoint_);
+
+      tracker_pose_tmp = tracker_setpoint_.pose.pose;
+    }
+
+    double          diff_x = std::abs(tracker_pose_tmp.position.x - rviz_nav_goal_tmp.pose.position.x);
+    double          diff_y = std::abs(tracker_pose_tmp.position.y - rviz_nav_goal_tmp.pose.position.y);
+    tf2::Quaternion q1, q2;
+    tf2::fromMsg(tracker_pose_tmp.orientation, q1);
+    tf2::fromMsg(rviz_nav_goal_tmp.pose.orientation, q2);
+    tf2::Quaternion rot_diff = q2 * q1.inverse();
+    double          diff_yaw = std::abs(mrs_lib::AttitudeConverter(rot_diff).getYaw());
+    /* ROS_INFO("diff: x: %2.4f y: %2.4f yaw: %2.4f\n", diff_x, diff_y, diff_yaw); */
+    if (diff_x < 0.1 && diff_y < 0.1 && diff_yaw < 0.1) {
+      break;
+    }
+
+    ros::Duration sleep_time(0.01);
+    sleep_time.sleep();
+    total_diag_sleep_time += sleep_time;
+  }
+
+  ROS_INFO("[NavGoal]: Waited %f s for tracker setpoint.", total_diag_sleep_time.toSec());
+
+  ROS_INFO("[NavGoal]: Flying to rviz nav goal:");
+  ROS_INFO("frame_id: %s\tx: %2.2f y: %2.2f z: %2.2f heading: %2.2f\n", new_waypoint.request.header.frame_id.c_str(), new_waypoint.request.reference.position.x,
+           new_waypoint.request.reference.position.y, new_waypoint.request.reference.position.z, new_waypoint.request.reference.heading);
+
+  got_odom_uav_         = false;
+  got_tracker_setpoint_ = false;
+}
+
+//}
+
+/* callbackOdomUav() //{ */
+
+void NavGoal::callbackOdomUav(const nav_msgs::OdometryConstPtr& msg) {
+
+  /* do not continue if the nodelet is not initialized */
+  if (!is_initialized_)
+    return;
+
+  {
+    std::scoped_lock lock(mutex_odom_uav_);
+    odom_uav_ = *msg;
+  }
+
+  if (!got_odom_uav_) {
+    got_odom_uav_ = true;
+  }
+}
+
+//}
+
+/* callbackTrackerSetpoint() //{ */
+
+void NavGoal::callbackTrackerSetpoint(const nav_msgs::OdometryConstPtr& msg) {
+
+  /* do not continue if the nodelet is not initialized */
+  if (!is_initialized_)
+    return;
+
+  {
+    std::scoped_lock lock(mutex_tracker_setpoint_);
+
+    tracker_setpoint_ = *msg;
+  }
+  got_tracker_setpoint_ = true;
+}
+
+//}
+
+}  // namespace rviz_interface
+
+}  // namespace mrs_rviz_plugins
+
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(mrs_rviz_plugins::rviz_interface::NavGoal, nodelet::Nodelet);
---
+++ src/rviz_interface/pose_estimate.cpp
@@ -0,0 +1,442 @@
+/* includes //{ */
+
+#include <ros/ros.h>
+#include <nodelet/nodelet.h>
+
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/PoseArray.h>
+
+#include <sensor_msgs/NavSatFix.h>
+
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+
+#include <mrs_lib/param_loader.h>
+#include <mrs_lib/profiler.h>
+#include <mrs_lib/transformer.h>
+#include <mrs_lib/mutex.h>
+
+#include <mrs_msgs/TransformPoseSrv.h>
+
+#include <std_srvs/Trigger.h>
+
+//}
+
+namespace mrs_rviz_plugins
+{
+
+namespace rviz_interface
+{
+
+/* class PoseEstimate //{ */
+
+class PoseEstimate : public nodelet::Nodelet {
+
+public:
+  virtual void onInit();
+
+private:
+  ros::NodeHandle nh_;
+  bool            is_initialized_ = false;
+
+private:
+  // subscribers and publishers
+  ros::Subscriber sub_rviz_pose_estimate_;
+  ros::Publisher  pub_pose_array_;
+  ros::Publisher  pub_marker_array_;
+
+  ros::Timer timer_pub_;
+
+  // publisher rate
+  int _pub_rate_;
+
+  // mutex for locking the position info
+  std::mutex mutex_rviz_pose_estimate_;
+  std::mutex mutex_pose_array_;
+  std::mutex mutex_utm_pose_array_;
+  std::mutex mutex_marker_array_;
+  std::mutex mutex_latlon_pose_array_;
+
+  bool got_rviz_pose_estimate_ = false;
+
+  // count of pose estimates
+  int count_;
+
+  // republished pose message
+  geometry_msgs::PoseWithCovarianceStamped rviz_pose_estimate_;
+  geometry_msgs::PoseArray                 pose_array_;
+  geometry_msgs::PoseArray                 utm_pose_array_;
+  geometry_msgs::PoseArray                 latlon_pose_array_;
+  visualization_msgs::MarkerArray          marker_array_;
+
+  bool               callbackDump(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res);
+  ros::ServiceServer srv_server_dump_;
+
+private:
+  void callbackRvizPoseEstimate(const geometry_msgs::PoseWithCovarianceStampedConstPtr &msg);
+  void timerPub(const ros::TimerEvent &event);
+
+private:
+  mrs_lib::Profiler profiler_;
+  bool              _profiler_enabled_ = false;
+};
+
+//}
+
+// --------------------------------------------------------------
+// |                      internal routines                     |
+// --------------------------------------------------------------
+
+/* onInit() //{ */
+
+void PoseEstimate::onInit() {
+
+  nh_ = nodelet::Nodelet::getMTPrivateNodeHandle();
+
+  mrs_lib::ParamLoader param_loader(nh_, "PoseEstimate");
+
+  param_loader.loadParam("enable_profiler", _profiler_enabled_);
+
+  param_loader.loadParam("publish_rate", _pub_rate_);
+
+  // SUBSCRIBERS
+  sub_rviz_pose_estimate_ = nh_.subscribe("rviz_pose_estimate_in", 1, &PoseEstimate::callbackRvizPoseEstimate, this, ros::TransportHints().tcpNoDelay());
+
+  // PUBLISHERS
+  pub_pose_array_   = nh_.advertise<geometry_msgs::PoseArray>("pose_array_out", 1);
+  pub_marker_array_ = nh_.advertise<visualization_msgs::MarkerArray>("marker_array_out", 1);
+
+  // --------------------------------------------------------------
+  // |                           timers                           |
+  // --------------------------------------------------------------
+
+  timer_pub_ = nh_.createTimer(ros::Rate(_pub_rate_), &PoseEstimate::timerPub, this);
+
+  // --------------------------------------------------------------
+  // |                          services                          |
+  // --------------------------------------------------------------
+
+  srv_server_dump_ = nh_.advertiseService("dump_in", &PoseEstimate::callbackDump, this);
+
+  // --------------------------------------------------------------
+  // |                          profiler                          |
+  // --------------------------------------------------------------
+
+  profiler_ = mrs_lib::Profiler(nh_, "PoseEstimate", _profiler_enabled_);
+
+  // | --------------------- tf transformer --------------------- |
+
+  // | ----------------------- finish init ---------------------- |
+
+  if (!param_loader.loadedSuccessfully()) {
+    ROS_ERROR("[PoseEstimate]: Could not load all parameters!");
+    ros::shutdown();
+  }
+
+  count_ = 0;
+
+  is_initialized_ = true;
+
+  ROS_INFO("[PoseEstimate]: initialized");
+
+  ROS_INFO("[PoseEstimate]: Waiting for user input in rviz...");
+}
+
+//}
+
+// --------------------------------------------------------------
+// |                          callbacks                         |
+// --------------------------------------------------------------
+
+/* callbackRvizPoseEstimate() //{ */
+
+void PoseEstimate::callbackRvizPoseEstimate(const geometry_msgs::PoseWithCovarianceStampedConstPtr &msg) {
+
+  if (!is_initialized_)
+    return;
+
+  mrs_lib::Routine profiler_routine = profiler_.createRoutine("callbackRvizPoseEstimate");
+
+  got_rviz_pose_estimate_ = true;
+
+  count_++;
+
+  {
+    std::scoped_lock lock(mutex_rviz_pose_estimate_);
+
+    rviz_pose_estimate_ = *msg;
+  }
+
+  // Get uav name of the pose estimate frame
+  size_t      idx_slash = msg->header.frame_id.find("/");
+  std::string uav_name  = msg->header.frame_id.substr(0, idx_slash);
+
+  // Prepare transformer
+  ros::ServiceClient         srv_client_transform_pose = nh_.serviceClient<mrs_msgs::TransformPoseSrv>("/" + uav_name + "/control_manager/transform_pose");
+  mrs_msgs::TransformPoseSrv transform_pose_srv;
+
+  geometry_msgs::Pose new_pose;
+  new_pose.position    = msg->pose.pose.position;
+  new_pose.orientation = msg->pose.pose.orientation;
+
+  {
+    std::scoped_lock lock(mutex_pose_array_);
+
+    pose_array_.poses.push_back(new_pose);
+    pose_array_.header.stamp    = ros::Time::now();
+    pose_array_.header.frame_id = msg->header.frame_id;
+  }
+
+  // rviz pose estimate marker
+  visualization_msgs::Marker marker;
+  marker.header.frame_id = uav_name + "/gps_origin";
+  marker.type            = visualization_msgs::Marker::CUBE_LIST;
+  marker.ns              = "2d_rviz_pose_estimate";
+  marker.id              = count_;
+  marker.color.a         = 1;
+  marker.scale.x         = 0.2;
+  marker.scale.y         = 0.2;
+  marker.scale.z         = 0.2;
+  marker.color.r         = 1;
+  marker.color.g         = 0;
+  marker.color.b         = 0;
+
+  geometry_msgs::Point pt_tmp;
+  pt_tmp.x = new_pose.position.x;
+  pt_tmp.y = new_pose.position.y;
+  pt_tmp.z = new_pose.position.z;
+  marker.points.push_back(pt_tmp);
+
+  marker_array_.markers.push_back(marker);
+
+  visualization_msgs::Marker text_marker;
+  text_marker.header.frame_id = uav_name + "/gps_origin";
+  text_marker.type            = visualization_msgs::Marker::TEXT_VIEW_FACING;
+  text_marker.ns              = "2d_rviz_pose_estimate_text";
+  text_marker.id              = count_;
+  text_marker.pose            = new_pose;
+  text_marker.pose.position.z += 0.5;
+  text_marker.color.a = 1;
+  text_marker.scale.z = 0.2;
+  text_marker.color.r = 1;
+  text_marker.color.g = 0;
+  text_marker.color.b = 0;
+  std::stringstream ss;
+  ss << "id: " << count_ << "\nx: " << pt_tmp.x << "\ny: " << pt_tmp.y;
+  text_marker.text = ss.str();
+
+  marker_array_.markers.push_back(text_marker);
+
+  geometry_msgs::Quaternion orientation_tmp;
+  double                    yaw_tmp;
+
+  // Current rviz frame
+  orientation_tmp = msg->pose.pose.orientation;
+  yaw_tmp         = mrs_lib::AttitudeConverter(orientation_tmp).getYaw();
+
+  ROS_INFO("[PoseEstimate]: New rviz 2D pose estimate:");
+  ROS_INFO("[PoseEstimate]: frame_id: %s\nx: %f y: %f yaw: %f", msg->header.frame_id.c_str(), msg->pose.pose.position.x, msg->pose.pose.position.y, yaw_tmp);
+
+  // GPS frame
+  if (msg->header.frame_id != uav_name + "/gps_origin") {
+
+    geometry_msgs::PoseStamped gps_pose;
+    gps_pose.pose            = new_pose;
+    gps_pose.header.frame_id = msg->header.frame_id;
+    gps_pose.header.stamp    = ros::Time::now();
+
+    transform_pose_srv.request.frame_id = "gps_origin";
+    transform_pose_srv.request.pose     = gps_pose;
+
+    /* ROS_INFO("[PoseEstimate]: Calling transformer service: %s", ("/" + uav_name + "/control_manager/transform_pose").c_str()); */
+
+    srv_client_transform_pose.call(transform_pose_srv);
+
+    /* ROS_INFO("[PoseEstimate]: Transformer service response: %s", transform_pose_srv.response.message.c_str()); */
+
+    if (transform_pose_srv.response.success) {
+      gps_pose = transform_pose_srv.response.pose;
+    } else {
+      ROS_ERROR("[PoseEstimate]: Could not transform to gps_origin.");
+      return;
+    }
+
+    orientation_tmp = gps_pose.pose.orientation;
+    yaw_tmp         = mrs_lib::AttitudeConverter(orientation_tmp).getYaw();
+
+    ROS_INFO("[PoseEstimate]: frame_id: %s\nx: %f y: %f yaw: %f", gps_pose.header.frame_id.c_str(), gps_pose.pose.position.x, gps_pose.pose.position.y,
+             yaw_tmp);
+  }
+
+  // UTM frame
+  geometry_msgs::PoseStamped utm_pose;
+  utm_pose.pose            = new_pose;
+  utm_pose.header.frame_id = msg->header.frame_id;
+  utm_pose.header.stamp    = ros::Time::now();
+
+  transform_pose_srv.request.frame_id = "utm_origin";
+  transform_pose_srv.request.pose     = utm_pose;
+
+  /* ROS_INFO("[PoseEstimate]: Calling transformer service: %s", ("/" + uav_name + "/control_manager/transform_pose").c_str()); */
+
+  srv_client_transform_pose.call(transform_pose_srv);
+
+  /* ROS_INFO("[PoseEstimate]: Transformer service response: %s", transform_pose_srv.response.message.c_str()); */
+
+  if (transform_pose_srv.response.success) {
+    utm_pose = transform_pose_srv.response.pose;
+  } else {
+    ROS_ERROR("[PoseEstimate]: Could not transform to utm_origin.");
+    return;
+  }
+  orientation_tmp = utm_pose.pose.orientation;
+  yaw_tmp         = mrs_lib::AttitudeConverter(orientation_tmp).getYaw();
+
+  ROS_INFO("[PoseEstimate]: frame_id: %s\nx: %f y: %f yaw: %f", utm_pose.header.frame_id.c_str(), utm_pose.pose.position.x, utm_pose.pose.position.y, yaw_tmp);
+
+  {
+    std::scoped_lock lock(mutex_utm_pose_array_);
+
+    utm_pose_array_.poses.push_back(utm_pose.pose);
+    utm_pose_array_.header.stamp    = ros::Time::now();
+    utm_pose_array_.header.frame_id = utm_pose.header.frame_id;
+  }
+
+  // LatLon frame
+  geometry_msgs::PoseStamped latlon_pose;
+  latlon_pose.pose            = new_pose;
+  latlon_pose.header.frame_id = msg->header.frame_id;
+  latlon_pose.header.stamp    = ros::Time::now();
+
+  transform_pose_srv.request.frame_id = "latlon_origin";
+  transform_pose_srv.request.pose     = latlon_pose;
+
+  srv_client_transform_pose.call(transform_pose_srv);
+
+  if (transform_pose_srv.response.success) {
+    latlon_pose = transform_pose_srv.response.pose;
+  } else {
+    ROS_ERROR("[PoseEstimate]: Could not transform to latlon_origin.");
+    return;
+  }
+
+  orientation_tmp = latlon_pose.pose.orientation;
+  yaw_tmp         = mrs_lib::AttitudeConverter(orientation_tmp).getYaw();
+
+  ROS_INFO("[PoseEstimate]: frame_id: %s\nlatitude: %f longitude: %f yaw: %f", latlon_pose.header.frame_id.c_str(), latlon_pose.pose.position.x,
+           latlon_pose.pose.position.y, yaw_tmp);
+  {
+    std::scoped_lock lock(mutex_latlon_pose_array_);
+
+    latlon_pose_array_.poses.push_back(latlon_pose.pose);
+    latlon_pose_array_.header.stamp    = ros::Time::now();
+    latlon_pose_array_.header.frame_id = latlon_pose.header.frame_id;
+  }
+}
+
+//}
+
+// --------------------------------------------------------------
+// |                           timers                           |
+// --------------------------------------------------------------
+
+/* timerPub() //{ */
+
+void PoseEstimate::timerPub(const ros::TimerEvent &event) {
+
+  if (!is_initialized_)
+    return;
+
+  mrs_lib::Routine profiler_routine = profiler_.createRoutine("timerPub", _pub_rate_, 0.01, event);
+
+  if (!got_rviz_pose_estimate_)
+    return;
+
+  geometry_msgs::PoseArray pose_array_out;
+
+  {
+    std::scoped_lock lock(mutex_pose_array_);
+
+    pose_array_out = pose_array_;
+  }
+
+  try {
+    pub_pose_array_.publish(pose_array_out);
+  }
+  catch (...) {
+    ROS_ERROR("[PoseEstimate]: Exception caught during publishing topic %s.", pub_pose_array_.getTopic().c_str());
+  }
+
+  visualization_msgs::MarkerArray marker_array_out;
+
+  {
+    std::scoped_lock lock(mutex_marker_array_);
+
+    marker_array_out = marker_array_;
+  }
+
+  try {
+    pub_marker_array_.publish(marker_array_out);
+  }
+  catch (...) {
+    ROS_ERROR("[PoseEstimate]: Exception caught during publishing topic %s.", pub_marker_array_.getTopic().c_str());
+  }
+}
+
+//}
+
+/* callbackDump() //{ */
+
+bool PoseEstimate::callbackDump([[maybe_unused]] std_srvs::Trigger::Request &req, [[maybe_unused]] std_srvs::Trigger::Response &res) {
+
+  if (!is_initialized_) {
+
+    ROS_WARN("[PoseEstimate]: Cannot dump coordinates, nodelet is not initialized.");
+    res.message = "Cannot dump coordinates, nodelet is not initialized.";
+    res.success = false;
+    return true;
+  }
+
+  if (!got_rviz_pose_estimate_) {
+    ROS_WARN("[PoseEstimate]: Cannot dump coordinates, no point available.");
+    res.message = "Cannot dump coordinates, no point available.";
+    res.success = false;
+    return true;
+  }
+
+  ROS_INFO("[PoseEstimate]: UTM pose array dump:");
+  std::stringstream ss;
+  ss << "UTM: ";
+  {
+    std::scoped_lock lock(mutex_utm_pose_array_);
+
+    for (size_t i = 0; i < utm_pose_array_.poses.size(); i++) {
+      std::printf("[%.6f, %.6f]\n", utm_pose_array_.poses[i].position.x, utm_pose_array_.poses[i].position.y);
+      ss << "[" << utm_pose_array_.poses[i].position.x << ", " << utm_pose_array_.poses[i].position.x << "] ";
+    }
+  }
+
+  ROS_INFO("[PoseEstimate]: LatLon pose array dump:");
+  ss << "LatLon: ";
+  {
+    std::scoped_lock lock(mutex_latlon_pose_array_);
+
+    for (size_t i = 0; i < latlon_pose_array_.poses.size(); i++) {
+      std::printf("[%.6f, %.6f]\n", latlon_pose_array_.poses[i].position.x, latlon_pose_array_.poses[i].position.y);
+      ss << "[" << latlon_pose_array_.poses[i].position.x << ", " << latlon_pose_array_.poses[i].position.x << "] ";
+    }
+  }
+
+  res.message = ss.str();
+  res.success = true;
+  return true;
+}
+
+//}
+
+}  // namespace rviz_interface
+
+}  // namespace mrs_rviz_plugins
+
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(mrs_rviz_plugins::rviz_interface::PoseEstimate, nodelet::Nodelet)
