35fc5357044e66f89089d26a18fcd37cadb333ad
Vaclav Pritzl <pritzvac@fel.cvut.cz>
2023-06-16 11:41:35 +0200

    added NamedSetGoal RViz tool

---
+++ src/named_goal_tool/named_goal_tool.cpp
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2008, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <geometry_msgs/PoseStamped.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
+#include <rviz/display_context.h>
+#include <rviz/ogre_helpers/arrow.h>
+/* #include <rviz/properties/string_property.h> */
+
+/* #include <mrs_rviz_plugins/named_goal_tool.h> */
+#include "named_goal_tool/named_goal_tool.h"
+
+namespace mrs_rviz_plugins
+{
+NamedGoalTool::NamedGoalTool() {
+  shortcut_key_ = 'g';
+
+  topic_property_ =
+      new rviz::StringProperty("Topic", "goal", "The topic on which to publish navigation goals.", getPropertyContainer(), SLOT(updateTopic()), this);
+  name_property_ = new rviz::StringProperty("Name", "name", "Name of the tool.", getPropertyContainer(), SLOT(updateName()), this);
+}
+
+void NamedGoalTool::onInitialize() {
+  PoseTool::onInitialize();
+  arrow_->setColor(1.0f, 0.0f, 1.0f, 1.0f);
+  setName("Custom Goal");
+  updateTopic();
+}
+
+void NamedGoalTool::updateTopic() {
+  try {
+    pub_ = nh_.advertise<geometry_msgs::PoseStamped>(topic_property_->getStdString(), 1);
+  }
+  catch (const ros::Exception& e) {
+    ROS_ERROR_STREAM_NAMED("NamedGoalTool", e.what());
+  }
+}
+
+void NamedGoalTool::updateName() {
+  try {
+    setName(name_property_->getStdString().c_str());
+  }
+  catch (const ros::Exception& e) {
+    ROS_ERROR_STREAM_NAMED("NamedGoalTool", e.what());
+  }
+}
+
+void NamedGoalTool::onPoseSet(double x, double y, double theta) {
+  std::string     fixed_frame = context_->getFixedFrame().toStdString();
+  tf2::Quaternion quat;
+  quat.setRPY(0.0, 0.0, theta);
+  geometry_msgs::PoseStamped goal;
+  goal.pose.orientation = tf2::toMsg(quat);
+  goal.pose.position.x  = x;
+  goal.pose.position.y  = y;
+  goal.header.frame_id  = fixed_frame;
+  goal.header.stamp     = ros::Time::now();
+  ROS_INFO(
+      "Setting goal to topic \'%s\': Frame:%s, Position(%.3f, %.3f, %.3f), Orientation(%.3f, %.3f, %.3f, %.3f) = "
+      "Angle: %.3f\n",
+      topic_property_->getStdString().c_str(), fixed_frame.c_str(), goal.pose.position.x, goal.pose.position.y, goal.pose.position.z, goal.pose.orientation.x,
+      goal.pose.orientation.y, goal.pose.orientation.z, goal.pose.orientation.w, theta);
+  pub_.publish(goal);
+}
+
+}  // end namespace mrs_rviz_plugins
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(mrs_rviz_plugins::NamedGoalTool, rviz::Tool)
